23dfa11ca7537044fea593ccd8f7c925c547ef4f
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-09-12 18:29:50 +0530

    Updates

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import AddCake from "./pages/cake_management/AddCake";
 import CakeList from "./pages/cake_management/CakeList";
+import EditCake from "./pages/cake_management/EditCake";
 import Login from "./pages/Login/Login";
 import AdminLayout from "./components/AdminLayout/AdminLayout";
 import Dashboard from "./components/Dashboard/Dashboard";
@@ -20,6 +21,7 @@ const App = () => {
             <Route index element={<Dashboard />} />
             <Route path="addcake" element={<AddCake />} />
             <Route path="cakelist" element={<CakeList />} />
+            <Route path="editcake/:id" element={<EditCake />} />
           </Route>
         </Routes>
       </div>
